9ecba11274e1c495d7540a5ccd57715a92ccf6d6
oferdrr8 <oferdrr8@gmail.com>
2022-05-30 16:52:27 +0300

    div css

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -3,11 +3,11 @@ const Note = ({id,text,date , handeleDelete}) => {
 
 
 
-<div className="note">
+<div className="note fs-4">
    
 <span className=""> {text}</span>
 
-<div className="note-footer">
+<div className="note-footer fs-6">
            <small>{date}</small>
            {/* <i className="fa-solid fa-trash-xmark mx-2 "></i> */}
      <i className="devicon-codecov-plain mx-2" onClick={()=>{
